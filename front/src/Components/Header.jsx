d63f83bf13165c39f5f78ee6cbe687c7c6570d1a
AqueleCaio <106452341+AqueleCaio@users.noreply.github.com>
2023-07-04 00:56:50 -0300

    estrutura html do header finalizada
    estilização do componente header está em andamento

--- front/src/Components/Header.jsx
+++ front/src/Components/Header.jsx
@@ -1,16 +1,27 @@
 import '../Styles/Header.css'
-import lupa from '../imagens/Icon.png'
+import lupa from '../imagens/lupa.png'
+import casa from '../imagens/casa.png'
+import estrela from '../imagens/estrela.png'
+import sino from '../imagens/sino.png'
 
 export default function(){
     return(
-    
-        <header className ="search-container">
-        <form action="search.csv">
-        <input type="text" placeholder="Pesquisar..." name="search"/>
-        <img src= {lupa}/>
-        </form>
-        </header>
-        
-    )
+        <header className='header'>
+         <img className='logo' src="#" alt="Logo do Logali" />
 
+         <form action="#" method="get" autocomplete="off">
+            <input type="search" name="searchbar" id="i_search" />
+            <button type="submit"> <img src={lupa} alt="Icone de lupa" /> </button>
+
+         </form>
+
+         <picture>
+            <img src={casa} alt="Icone de Casa" />      {/*Home */}
+            <img src={estrela} alt="Icone de rlala" />  {/*Favoritos*/}
+            <img src={sino} alt="Icone de Sino" />      {/*Notificações*/}
+         </picture>
+
+         <button type="button"> <img class="img_perfil" src="#" alt="Imagem do Perfil" /> </button>
+        </header>
+    )
 }
